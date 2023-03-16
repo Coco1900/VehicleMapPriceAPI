8a5553d5d65009849d01d17228c6f67b5f9d6764
Coco1900 <46676720+Coco1900@users.noreply.github.com>
2023-03-16 11:33:39 +0100

    feat: price fonctionnel

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 8100;
+
+const authorised = '*'; // http://localhost:4200';
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', authorised);
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, Identification');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next(); 
+});
+
+
+app.use(bodyParser.json());
+
+
+app.get('/:km', (req, res) => {
+    const km = parseFloat(req.params.km);
+    const price = km * 0.208;
+    res.send({ price });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
